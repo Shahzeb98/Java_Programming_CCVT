bd4c7a8e8dc8c789fa4c061462fa067bb52e20c5
Shahzeb <Shahzeb@DESKTOP-9V794N5>
2018-02-19 19:08:48 +0530

    These changes are done by Shahzeb Rizvi R110216143 CCVT 2nd year

--- CCVT_Code_Distribute/src/com/rt/cloning/TestClass.java
+++ CCVT_Code_Distribute/src/com/rt/cloning/TestClass.java
@@ -8,8 +8,8 @@ public class TestClass {
 	 */
 	public static void main(String[] args) throws CloneNotSupportedException {
 		// TODO Auto-generated method stub
-	Department d1=new Department(5, "MD");
-	Employee e1=new Employee(3, "Sham",d1);
+	Department d1=new Department(5, "MD"); // Change from 7 to 5 and HR to MD
+	Employee e1=new Employee(3, "Sham",d1); // 1 to 3 from Ram to Sham
 	Employee e2= e1.clone();
 	
 
@@ -17,9 +17,9 @@ public class TestClass {
 	System.out.println(e1);
 	System.out.println(e2);
 	e2.setEid(3);
-	e2.setEname("Harry");
+	e2.setEname("Harry"); // Shyam to Harry
 	e2.getDep().setDid(8);
-	e2.getDep().setDname("Math");
+	e2.getDep().setDname("Math"); // Acc to Math
 	System.out.println(e1);
 	System.out.println(e2);
 
